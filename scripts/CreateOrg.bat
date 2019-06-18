a9096ef4939795d86147b13d1192706f245b2d29
Justin Lyon <jml6487@gmail.com>
2019-06-18 08:41:24 -0500

    add scratchorg name input to CreateOrg.bat

--- scripts/CreateOrg.bat
+++ scripts/CreateOrg.bat
@@ -1,9 +1,23 @@
-REM Execute in PC using: ./scripts/CreateOrg.bat
+SETLOCAL EnableExtensions EnableDelayedExpansion
+@ECHO off
+
+SET scratchOrgName=%1
+ECHO %scratchOrgName%
+
+if "%scratchOrgName%"=="" (
+    ECHO scratchOrgName is NULL. Expecting first argument string name.
+    EXIT /B 5
+)
+
+
+REM REM Execute in PC using: ./scripts/CreateOrg.bat
 echo "*** Creating scratch Org..."
-call sfdx force:org:create -s -a hello-lwc -d 30 -f ../config/project-scratch-def.json
+call sfdx force:org:create -s -a %scratchOrgName% -d 30 -f ../config/project-scratch-def.json
 echo "*** Pushing metadata to scratch Org..."
 call sfdx force:source:push
 echo "*** Creating data from Account-Contact-plan.json"
-call sfdx force:data:tree:import -u hello-lwc -p ../data/Account-Contact-plan.json
+call sfdx force:data:tree:import -u %scratchOrgName% -p ../data/Account-Contact-plan.json
 echo "*** Assigning TrainingApp Permission Set"
-call sfdx force:user:permset:assign -n TrainingApp -u hello-lwc
+call sfdx force:user:permset:assign -n TrainingApp -u %scratchOrgName%
+
+ENDLOCAL
