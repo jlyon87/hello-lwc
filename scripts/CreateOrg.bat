a2bbb2ee91fd7a2baab7801d8fa1a61de3f3be30
Justin Lyon <jml6487@gmail.com>
2019-06-18 07:51:48 -0500

    add 30day duration to scripts

--- scripts/CreateOrg.bat
+++ scripts/CreateOrg.bat
@@ -1,6 +1,6 @@
 REM Execute in PC using: ./scripts/CreateOrg.bat
 echo "*** Creating scratch Org..."
-call sfdx force:org:create -s -a hello-lwc -f ../config/project-scratch-def.json
+call sfdx force:org:create -s -a hello-lwc -d 30 -f ../config/project-scratch-def.json
 echo "*** Pushing metadata to scratch Org..."
 call sfdx force:source:push
 echo "*** Creating data from Account-Contact-plan.json"
--- scripts/CreateOrg.sh
+++ scripts/CreateOrg.sh
@@ -1,6 +1,6 @@
 # Execute in Mac using: ./scripts/CreateOrg.sh
 echo "*** Creating scratch Org..."
-sfdx force:org:create -f config/project-scratch-def.json -s -u hello-lwc -f config/project-scratch-def.json
+sfdx force:org:create -f config/project-scratch-def.json -s -u hello-lwc -d 30 -f config/project-scratch-def.json
 echo "*** Pushing metadata to scratch Org..."
 sfdx force:source:push
 echo "*** Creating data from Account-Contact-plan.json"
